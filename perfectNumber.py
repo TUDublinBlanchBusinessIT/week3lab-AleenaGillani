b0f5d90121807d6f65d05d71edc641a7e4340e4a
B00152372 Aleena Gillani <B00152372@mytudublin.ie>
2024-02-07 12:11:08 +0000

    findFirstFourPNs.py running

--- perfectNumber.py
+++ perfectNumber.py
@@ -2,17 +2,13 @@
 #07/07/2024
 #perfectNumber.py
 
-#A perfect number is one for which all the divisors of the number add up to the
-#number itself. For example the divisors of 28 are 1,2,4,7,14 which added together gives 28
-#write a function below called perfectNumber(x) which checks to see if x is a perfect number
-#The function should return True if the number is perfect and False if it is not
-
+                                
 from divisors import divisors
-#define the function header called perfectNumber expecting one argument
+
 
 def perfectNumber(x):
     result = False
-    sum_divisor = sum(divisor(x))
-    if sum_divisor == x:
+    sum_of_divisors = sum(divisors(x))
+    if sum_of_divisors == x:
         result = True
     return result
